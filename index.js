85700a2d02285fa49315da8740b931ae5d43bb9e
Amandine Perichon <apericho@gmail.com>
2016-08-11 21:11:21 +1200

    Add one line based on a shapes list

--- index.js
+++ index.js
@@ -3,13 +3,41 @@ import ReactDOM from 'react-dom'
 
 const DiagramEditor = React.createClass({
   propTypes: {
-
+    shapes: React.PropTypes.array.isRequired
   },
-  render: () => {
-    return <p>
-        Trying the config
-      </p>
+  render: function () {
+    const Type = this.props.shapes[0].type
+    const shape = <Type
+      {...this.props.shapes[0].attributes} />
+
+    return <svg height="500" width="500">{shape}</svg>
   }
 })
 
-ReactDOM.render(<DiagramEditor />, document.getElementById('app'))
+const SVGLine = React.createClass({
+  propTypes: {
+    x1: React.PropTypes.number.isRequired,
+    y1: React.PropTypes.number.isRequired,
+    x2: React.PropTypes.number.isRequired,
+    y2: React.PropTypes.number.isRequired
+  },
+  render: function () {
+    return <line 
+      {...this.props}
+      stroke="black" />
+  }
+})
+
+var shapes = [
+  {
+    type: SVGLine,
+    attributes: {
+      x1: 0,
+      y1: 0,
+      x2: 200,
+      y2: 200
+    }
+  }
+]
+
+ReactDOM.render(<DiagramEditor shapes={shapes}/>, document.getElementById('app'))
