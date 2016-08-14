bf933528a970b5e2e709c3732222a6855a31bd67
Amandine Perichon <apericho@gmail.com>
2016-08-14 16:31:50 +1200

    Change react components to be independant of Redux

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ import {buildShape} from './buildShape'
 
 const DiagramEditor = React.createClass({
   propTypes: {
-    state: React.PropTypes.object.isRequired
+    diagram: React.PropTypes.array.isRequired,
+    onChange: React.PropTypes.func.isRequired
   },
   getInitialState: function () {
     return {
@@ -18,7 +19,7 @@ const DiagramEditor = React.createClass({
     const tools = ['rectangle', 'ellipse', 'line', 'arrow', 'text']
     return <div className="diagram-editor" style={{width:800}}>
       <Toolbar selectedTool={this.state.selectedTool} onToolChange={this.onToolChange} tools={tools}/>
-      <Diagram selectedTool={this.state.selectedTool} shapes={this.props.state.shapes}/>
+      <Diagram selectedTool={this.state.selectedTool} shapes={this.props.diagram} onChange={this.props.onChange}/>
       </div>
   },
   onToolChange: function (evt) {
@@ -45,7 +46,8 @@ const Toolbar = React.createClass({
 const Diagram = React.createClass({
   propTypes: {
     selectedTool: React.PropTypes.string.isRequired,
-    shapes: React.PropTypes.array.isRequired
+    shapes: React.PropTypes.array.isRequired,
+    onChange: React.PropTypes.func.isRequired
   },
   render: function () {
     const canvas = this.props.shapes.map((elem, i) => {
@@ -56,19 +58,17 @@ const Diagram = React.createClass({
     return <svg height="600" width="800" onClick={this.useTool}>{canvas}</svg>
   },
   useTool: function (evt) {
-    console.log(this)
-    console.log('clicked, position: ', evt.nativeEvent.offsetX, evt.nativeEvent.offsetY, this.props.selectedTool, (buildShape(this.props.selectedTool, evt)))
-    store.dispatch({type: "ADD_SHAPE",
-      shape: (buildShape(this.props.selectedTool, evt))
-    })
+    console.log('clicked, position: ', evt.nativeEvent.offsetX, evt.nativeEvent.offsetY, this.props.selectedTool)
+    const newDiagram = [...this.props.shapes, buildShape(this.props.selectedTool, evt)]
+    this.props.onChange(newDiagram)
   }
 })
 
 // STORE
-const editorReducer = function (state = {shapes: []}, action) {
+const editorReducer = function (state = {diagram: []}, action) {
   switch(action.type) {
-    case 'ADD_SHAPE':
-      return Object.assign({}, {shapes: [...state.shapes, action.shape]})
+    case 'UPDATE_DIAGRAM':
+      return Object.assign({}, {diagram: action.diagram})
     default:
       return state
   }
@@ -79,6 +79,13 @@ refresh()
 
 store.subscribe(refresh)
 
+function changeDiagram (diagram) {
+  store.dispatch({type: "UPDATE_DIAGRAM", diagram: diagram})
+}
+
+
 function refresh() {
-  ReactDOM.render(<DiagramEditor state={store.getState()}/>, document.getElementById('app'))
+  ReactDOM.render(<DiagramEditor diagram={store.getState().diagram}
+                    onChange={changeDiagram}/>,
+                    document.getElementById('app'))
 }
