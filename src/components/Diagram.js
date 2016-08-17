97fbb11166a5e7b6e1c9474618578d0d8fc36be1
Amandine Perichon <apericho@gmail.com>
2016-08-17 20:45:39 +1200

    Refactoring Diagram in smaller functions

--- src/components/Diagram.js
+++ src/components/Diagram.js
@@ -13,11 +13,6 @@ export default React.createClass({
     }
   },
   render: function () {
-    const canvas = this.props.shapes.map((elem, i) => {
-      const Type = elem.type
-      return <Type {...elem.attributes} key={i}/>
-    })
-
     let temp = null
     if (this.state.action) {
       const tempData = buildShape(this.props.selectedTool, this.state.action)
@@ -25,32 +20,49 @@ export default React.createClass({
       temp = <TempType {...tempData.attributes} />
     }
 
-    return <div className="diagram">
-            <svg height="600" width="800" onMouseDown={this.initializeAction}
-                  onMouseMove={this.tempAction} onMouseUp={this.completeAction}>
-                    {canvas}{temp}
-            </svg>
-          </div>
+    return (
+      <div className="diagram">
+        <svg  height="600"
+              width="800"
+              onMouseDown={this.mouseDownAction}
+              onMouseMove={this.mouseMoveAction}
+              onMouseUp={this.mouseUpAction}>
+          {createCanvas(this.props.shapes)}
+          {temp}
+        </svg>
+      </div>
+    )
   },
-  initializeAction: function (evt) {
+  mouseDownAction: function (evt) {
     evt.preventDefault()
-    const initial = coord(evt)
-    this.setState({action: {initial: initial, final: initial}})
+    this.setState({action: {initial: coord(evt), final: coord(evt)}})
   },
-  tempAction: function (evt) {
+  mouseMoveAction: function (evt) {
     evt.preventDefault()
     if (this.state.action) {
-      const final = coord(evt)
-      const newAction = Object.assign({}, this.state.action, {final: final})
+      const newAction = Object.assign({}, this.state.action, {final: coord(evt)})
       this.setState({action: newAction})
     }
   },
-  completeAction: function (evt) {
-    const newDiagram = [...this.props.shapes, buildShape(this.props.selectedTool, this.state.action)]
-    this.props.onChange(newDiagram)
+  mouseUpAction: function (evt) {
+    if (creatingShapeAction(this.props.selectedTool)) {
+      const newDiagram = [...this.props.shapes, buildShape(this.props.selectedTool, this.state.action)]
+      this.props.onChange(newDiagram)
+    }
     this.setState({action: null})
   }
 })
+
+function creatingShapeAction (selectedTool) {
+  return ['rectangle', 'ellipse', 'line', 'arrow'].includes(selectedTool)
+}
+
+function createCanvas (shapes) {
+  return shapes.map((elem, i) => {
+    const Type = elem.type
+    return <Type {...elem.attributes} key={i}/>
+  })
+}
 
 function coord (evt) {
   return {
